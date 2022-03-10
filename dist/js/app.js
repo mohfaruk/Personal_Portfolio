d34a7fc2a8c8fb3eb8a2510317e7779cbef7af96
Mohamad Faruk <mohamadfaruk.uk~gmail.com>
2022-03-10 14:06:12 +0000

    hamburger navbar included

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,4 +1,21 @@
-//Text Effect
+//Type Effect
+
+//Hamburger
+const navMenu = document.querySelector(".nav-menu");
+const hamburger = document.querySelector(".hamburger");
+const navItems = document.querySelectorAll(".menu-item");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+navItems.forEach(item =>
+  item.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
 
 //Smooth Scroll
 const scrollUp = document.querySelector(".scroll-top");
